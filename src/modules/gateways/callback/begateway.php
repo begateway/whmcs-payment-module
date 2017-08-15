1a08fc36fc9c3308707e0da0b0af163183389395
avsh <avterus@gmail.com>
2017-08-15 08:54:54 +0300

    link to release

--- src/modules/gateways/callback/begateway.php
+++ src/modules/gateways/callback/begateway.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * Copyright (C) 2017 beGateway
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @author      beGateway
+ * @copyright   2017 beGateway
+ * @version     1.0.0
+ * @license     https://opensource.org/licenses/MIT The MIT License
+ */
 require "../../../init.php";
 require_once(__DIR__ . '/../begateway/lib/lib/beGateway.php');
 
