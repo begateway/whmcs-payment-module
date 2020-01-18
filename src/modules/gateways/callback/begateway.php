87a7519ce130bb30b61ab2edd4bb71eb96704637
avsh <avsh@users.noreply.github.com>
2020-01-18 15:25:19 +0300

    update payments for created invoice from "push" payment systems

--- src/modules/gateways/callback/begateway.php
+++ src/modules/gateways/callback/begateway.php
@@ -13,8 +13,8 @@
  * GNU General Public License for more details.
  *
  * @author      beGateway
- * @copyright   2017 beGateway
- * @version     1.0.0
+ * @copyright   2020 beGateway
+ * @version     2.5.0
  * @license     https://opensource.org/licenses/MIT The MIT License
  */
 require "../../../init.php";
@@ -22,42 +22,58 @@ require_once(__DIR__ . '/../begateway/lib/lib/beGateway.php');
 
 $whmcs->load_function("gateway");
 $whmcs->load_function("invoice");
-$GATEWAY = getGatewayVariables("begateway");
+$GATEWAY = getGatewayVariables($gatewayModuleName);
 
 if (!$GATEWAY['type']) {
-    $GATEWAY = getGatewayVariables("begateway");
-}
-
-if (!$GATEWAY['type']) {
-    exit("Module Not Activated");
+    die("Module Not Activated");
 }
 
 $webhook = new \BeGateway\Webhook;
 \BeGateway\Settings::$shopId = $GATEWAY['shop_id'];
 \BeGateway\Settings::$shopKey = $GATEWAY['shop_key'];
 
-$logs = print_r($webhook->getResponse(), true);
+$logs = json_encode($webhook->getResponseArray());
+
+logTransaction($GATEWAY['name'], $logs, $webhook->getStatus());
 
 if ($webhook->isAuthorized()) {
   list($invoiceid, $customerid) = explode('|',$webhook->getTrackingId());
+  $uid = $webhook->getUid();
 
-  $invoiceid = checkCbInvoiceID($invoiceid, "begateway");
+  # transaction has been made not via whmcs
+  if (!isset($customerid)) {
+    $invoiceid = null;
+  }
+
+  if (!isset($invoiceid) && isset($webhook->getResponse()->transaction->erip)) {
+    # try to get it from ERIP data
+    $invoiceid = $webhook->getResponse()->transaction->erip->account_number;
+    $invoiceid = preg_replace("/[^0-9]/", "", $invoiceid);
+    $invoiceid = intval($invoiceid);
+  }
+
+  $invoiceid = checkCbInvoiceID($invoiceid, $GATEWAY['name']);
+
+  # it halts script if trx exists and response body is empty
+
+  checkCbTransID($uid);
 
   if ($invoiceid && $webhook->isSuccess()) {
-      addInvoicePayment($invoiceid, $webhook->getUid(), "", "", "begateway");
-      if ($webhook->isTest()) {
-        begateway_add_note(array('invoiceid' => $invoiceid, 'transid' => $webhook->getUid()));
-      }
-      logTransaction("beGateway", $logs, "Successful");
+      $money = new \BeGateway\Money;
+      $money->setCurrency($webhook->getResponse()->transaction->currency);
+      $money->setCents($webhook->getResponse()->transaction->amount);
+
+      addInvoicePayment($invoiceid, $uid, $money->getAmount(), null, $gatewayModuleName);
+      begateway_add_note(array('invoiceid' => $invoiceid, 'transid' => $uid));
+
       if ($webhook->getResponse()->transaction->credit_card->token) {
         $cc = $webhook->getResponse()->transaction->credit_card;
         update_query("tblclients", array("cardtype" => $cc->brand, "gatewayid" => $cc->token, "cardlastfour" => $cc->last_4), array("id" => $customerid));
       }
 
-      header("location:../../../viewinvoice.php?id=" . $invoiceid . "&paymentsuccess=true");
       die('OK');
   }
-  logTransaction("beGateway", $logs, "Error");
+} else {
+  die('Not authorized');
 }
-header("location:../../../viewinvoice.php?id=" . $invoiceid . "&paymentfailed=true");
 die('ERROR');
