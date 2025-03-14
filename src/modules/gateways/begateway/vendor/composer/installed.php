f08785d4d24cd3d1b611206b1ce8b4bbc348da0a
avsh <avsh@users.noreply.github.com>
2025-03-14 14:50:13 +0300

    add composer

---
+++ src/modules/gateways/begateway/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'begateway/whmcs-payment-module',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'begateway/begateway-api-php' => array(
+            'pretty_version' => '5.1.1',
+            'version' => '5.1.1.0',
+            'reference' => '0238d5d45ee78cd02470fc22b75def380da345e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../begateway/begateway-api-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'begateway/whmcs-payment-module' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
