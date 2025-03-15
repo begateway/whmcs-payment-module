0d081074d34352718f411616b082101a718a5482
avsh <avsh@users.noreply.github.com>
2025-03-15 20:35:53 +0300

    add module params locales

---
+++ src/modules/gateways/begateway/lang/english.php
@@ -0,0 +1,20 @@
+<?php
+
+$_GATEWAYLANG = array();
+
+$_GATEWAYLANG['shop_id'] = "Shop ID";
+$_GATEWAYLANG['shop_id_desc'] = "Enter your shop Id";
+$_GATEWAYLANG['shop_key'] = "Shop Key";
+$_GATEWAYLANG['shop_key_desc'] = "Enter your shop secret key";
+$_GATEWAYLANG['domain_checkout'] = "Checkout Domain";
+$_GATEWAYLANG['domain_checkout_desc'] = "Enter your payment provider checkout domain e.g. checkout.domain.com";
+$_GATEWAYLANG['domain_gateway'] = 'Gateway Domain';
+$_GATEWAYLANG['domain_gateway_desc'] = 'Enter your payment provider gateway domain e.g. gateway.domain.com';
+$_GATEWAYLANG['card_enable'] = "Enable card payments";
+$_GATEWAYLANG['card_enable_desc'] = "Tick to enable card payments payments";
+$_GATEWAYLANG['erip_enable'] = "Enable ERIP";
+$_GATEWAYLANG['erip_enable_desc'] = "Tick to enable ERIP payments";
+$_GATEWAYLANG['erip_service_no'] = "ERIP service code";
+$_GATEWAYLANG['erip_service_no_desc'] = "Enter your ERIP service code";
+$_GATEWAYLANG['test_mode'] = "Enable test mode";
+$_GATEWAYLANG['test_mode_desc'] = "Tick to enable test mode";
